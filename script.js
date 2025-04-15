318bd2cf383f93f01d07c6c2dbbc450ea58bb5c9
davideerusso <rd.96.rd@gmail.com>
2025-04-15 10:58:27 +0200

    fine

--- script.js
+++ script.js
@@ -28,7 +28,15 @@ const pesoRandom = getRandomNumber(100, 350);
 for (const randomPeso of bike) {
   randomPeso.peso = getRandomNumber(100, 350);
 }
+let pesoBikeMin;
+for (const pesoMin of bike) {
+  if (pesoBikeMin === undefined || pesoMin.peso < pesoBikeMin.peso) {
+    pesoBikeMin = pesoMin;
+  }
+}
+
 console.log(bike);
+console.log(pesoBikeMin);
 
 // Snack2
 // Creare un array di oggetti di squadre di calcio.
@@ -36,3 +44,41 @@ console.log(bike);
 // Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+squadre = [
+  {
+    nome: "Milan",
+    puntiF: 0,
+    falliS: 0,
+  },
+  {
+    nome: "Juve",
+    puntiF: 0,
+    falliS: 0,
+  },
+  {
+    nome: "Inter",
+    puntiF: 0,
+    falliS: 0,
+  },
+];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+for (const currentTeam of squadre) {
+  currentTeam.puntiF = getRandomNumber(0, 40);
+  currentTeam.falliS = getRandomNumber(0, 20);
+}
+console.log(squadre);
+
+const nomiFalli = [];
+for (const team of squadre) {
+  const falliTeam = {
+    nome: team.nome,
+    falliS: team.falliS,
+  };
+  nomiFalli.push(falliTeam);
+}
+console.log(nomiFalli);
